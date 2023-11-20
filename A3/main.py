2f29baa4eed193eb72f8ae096b6287d1b462d243
StanimirMihaylovAngelov <146730769+StanimirMihaylovAngelov@users.noreply.github.com>
2023-11-20 14:46:18 +0100

    Update main.py
    Comments added

--- A3/main.py
+++ A3/main.py
@@ -1,8 +1,18 @@
+# Welcome
+# This script is created by Group 5 in the 'Advanced BIM'-course at DTU (Fall-2023)
+# This script creates a new CustomPropertySet for the standard reflectance of the walls, floors and ceilings, to prepair
+# the IFC moodels for a daylight analysis. The standard reflectances are from the ISO 17037.
+
+
+# Importing the Path-function + its library
 from pathlib import Path
+# Importing IFC OpenShell
 import ifcopenshell
 
+# Defineing the file name of the IFC model:
 modelname = "ARK_new"
 
+# Loading the IFC model by using the Path-function
 try:
     dir_path = Path(__file__).parent
     model_url = Path.joinpath(dir_path, 'model', modelname).with_suffix('.ifc')
@@ -20,7 +30,7 @@ except OSError:
 project = (model.by_type('IfcProject'))
 print(f'The loaded project is: {project[0].LongName}')
 
-
+# Defining the IfcProduct and IfcOwnerHistory
 products = model.by_type("IfcProduct")
 owner_history = model.by_type("IfcOwnerHistory")[0]
 
@@ -41,7 +51,7 @@ for wall in walls:
     model.createIfcRelDefinesByProperties(wall.GlobalId, owner_history, None, None, [wall], property_set)
 
 
-# ---------------- Slabs ----------------
+# ---------------- Slabs (Floors) ----------------
 
 Slabs=[]
 for i in products:
@@ -56,7 +66,7 @@ for Slab in Slabs:
     property_set = model.createIfcPropertySet(Slab.GlobalId, owner_history, "Custom Property Set", None, property_values)
     model.createIfcRelDefinesByProperties(Slab.GlobalId, owner_history, None, None, [Slab], property_set)
 
-# ---------------- Coverings ----------------
+# ---------------- Coverings (Ceilings) ----------------
 
 Coverings=[]
 for i in products:
@@ -71,6 +81,9 @@ for covering in Coverings:
     property_set = model.createIfcPropertySet(covering.GlobalId, owner_history, "Custom Property Set", None, property_values)
     model.createIfcRelDefinesByProperties(covering.GlobalId, owner_history, None, None, [covering], property_set)
 
-# Saving the new IFC-model
+
+# ----------------- Updating the IFC-file ------------------
+
+# Writing(saves) updated IFC-file to disk
 model.write(model_url)  
 print("Done - Reload the model")
